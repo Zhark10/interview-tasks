9e0676ad7640801687d8bd2708c6b52043bc922f
Zharavin Arkady <53864466+Zhark10@users.noreply.github.com>
2020-12-31 02:14:58 +0300

    Update killer_found.js

--- killer_found.js
+++ killer_found.js
@@ -1,21 +1,21 @@
-const oportunityKillers = {
+const possibleKillers = {
   'JACOB': ["STEPAN", "VERA"],
   'JACK': ["STEPAN", "ALEX", "MIKE"],
 }
 const deaded = ["STEPAN", "ALEX"]
-const killer = (_oportunityKillers, _deaded) => {
+const findTheKiller = (_possibleKillers, _deaded) => {
   let detectedKiller;
-  Object.keys(_oportunityKillers).forEach((currentOportunityKiller) => {
+  Object.keys(_possibleKillers).forEach((currentPossibleKiller) => {
  
-  const meetPeople = _oportunityKillers[currentOportunityKiller]
+  const meetPeople = _possibleKillers[currentPossibleKiller]
   if (detectedKiller) return
 
   if (_deaded.every(deadMan => meetPeople.includes(deadMan))) {
-      detectedKiller = currentOportunityKiller
+      detectedKiller = currentPossibleKiller
     }
   })
   return detectedKiller
 }
 
-console.log(killer(oportunityKillers, deaded))
+console.log(findTheKiller(possibleKillers, deaded))
 
