33b64dafefa2295e0febfd916e1e4652983baab3
Zhark10 <verygoodman18@yandex.ru>
2022-05-24 15:18:49 +0300

    upd bubble sort

--- algorithms/bubble_sort.js
+++ algorithms/bubble_sort.js
@@ -1,19 +1,19 @@
-const array = [1, 4, 5, 5, 3, 2, 4, 4, 345234, 456, 3, 32, 235, 45, 2, 123, -124, 36, -224]
+const nums = [1, 4, 5, 5, 3, 2, 4, 4, 345234, 456, 3, 32, 235, 45, 2, 123, -124, 36, -224]
 let countOfIterations = 0
 
-const bubbleSort = (data) => {
-  for (let i = 0; i < data.length; i++) {
-    for (let j = 0; j < data.length; j++) {
-      if (data[j] > data[j + 1]) {
-        let tmp = data[j]
-        data[j] = data[j + 1]
-        data[j + 1] = tmp
+const bubbleSort = arr => {
+  arr.forEach(() => {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j + 1] < arr[j]) {
+        let coppiedCurrentItem = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = coppiedCurrentItem
       }
-      countOfIterations += 1
+      countOfIterations++
     }
-  }
-  return data
+  })
+  return arr
 }
 
-console.log(bubbleSort(array))
+console.log(bubbleSort(nums))
 console.log('COUNT', countOfIterations)
