a6ea824b9061a3c95129ee999eeb94398978c31c
Zhark10 <verygoodman18@yandex.ru>
2022-05-24 15:07:31 +0300

    upd selection sort

--- algorithms/selection_sort.js
+++ algorithms/selection_sort.js
@@ -1,22 +1,24 @@
-const array = [2, 4, 4, 5, 3, 23, 4, 345, 23, 123, 4, 45, 23, 12, -23, -34, -65]
+const nums = [2, 4, 4, 5, 3, 23, 4, 345, 23, 123, 4234, 45, 23, 12, -23, -34, -605]
 let countOfIterations = 0
 
-const selectionSort = (data) => {
-  for (let i = 0; i < data.length; i++) {
-
-    let indexMin = i
-    for (let j = i + 1; j < data.length; j++) {
-      if(data[j] < data[indexMin]) {
-        indexMin = j
-      }
-      countOfIterations += 1
+const selectionSort = arr => {
+    for (let i = 0; i < arr.length; i++) {
+        let minInd = i
+        for (let j = i + 1; j < arr.length; j++) {
+            // upd min index
+            if (arr[j] < arr[minInd]) minInd = j
+            // for logger
+            countOfIterations++
+        }
+        // skip iteration
+        if (arr[i] === arr[minInd]) continue
+        // change positions
+        let tmp = arr[i]
+        arr[i] = arr[minInd]
+        arr[minInd] = tmp
     }
-    let tmp = data[i]
-    data[i] = data[indexMin]
-    data[indexMin] = tmp
-  }
-  return array
+    return arr
 }
 
-console.log(selectionSort(array))
+console.log(selectionSort(nums))
 console.log('COUNT', countOfIterations)
